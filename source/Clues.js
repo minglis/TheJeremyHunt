e64343a881e2226903174910ddc06e6b7421fedd
james-rodgers <james.rodgers@guardian.co.uk>
2011-07-10 15:10:49 +0100

    merged in audio

--- source/Clues.js
+++ source/Clues.js
@@ -3,9 +3,9 @@ enyo.kind({
     kind: enyo.Control,
     currentClueNumber: 0,
     cluelist: [
-	{text: "Mr M is coming to town to sort things out. He's arriving from Paris... ", lat: "51.53", lon: "-0.12", image: 'images/question_1.png'},
-      	{text: "You need to speak to Ms Brooks. We've had a tip-off you'll find her seeking solace in the bottle.", lat: "51.52", lon: "-0.12", image: 'images/question_2.png'},
-      	{text: "If only there was a place you could go to find a more honest and truthful account of events... ", lat: "51.53", lon: "-0.12", image: 'images/question_3.png'},
+	{text: "Mr M is coming to town to sort things out. He's arriving from Paris... ", lat: "51.53", lon: "-0.12", image: 'images/question_1.png', sound: 'resources/audio_1.mp3'},
+      	{text: "You need to speak to Ms Brooks. We've had a tip-off you'll find her seeking solace in the bottle.", lat: "51.52", lon: "-0.12", image: 'images/question_2.png', sound: 'resources/audio_2.mp3'},
+      	{text: "If only there was a place you could go to find a more honest and truthful account of events... ", lat: "51.53", lon: "-0.12", image: 'images/question_3.png', sound: 'resources/audio_3.mp3'},
 	
       	{text: "You've won!!", lat: "51.53", lon: "-0.12", image: 'images/win.png'}
     ],
--- source/TestLayout.js
+++ source/TestLayout.js
@@ -1,22 +1,23 @@
 enyo.kind({
-	currentTry : "firstTry",
+    currentTry : "firstTry",
     name: "MyApps.TestLayout",
     kind: enyo.VFlexBox,
     components: [
 	{
-    	  name : "getCurPosition",
-    	  kind: "PalmService",
-    	  service : "palm://com.palm.location/",
-    	  method : "getCurrentPosition",
-    	  onSuccess : "posFinished",
-    	  onFailure : "posFail",
-    	  onResponse : "gotResponse",
-    	  subscribe : false
-      },
+    	    name : "getCurPosition",
+    	    kind: "PalmService",
+    	    service : "palm://com.palm.location/",
+    	    method : "getCurrentPosition",
+    	    onSuccess : "posFinished",
+    	    onFailure : "posFail",
+    	    onResponse : "gotResponse",
+    	    subscribe : false
+	},
 	{kind: 'Clues'},
 	{kind: "PageHeader", content: "The Jeremy Hunt"},
+	{kind: "Sound", name: 'soundPlayer', src: 'resources/audio_1.mp3'},
 	
-	{kind: "Group", name: 'clueText', caption: "How many mice live under the tree to the north of the bus?",
+	{kind: "Group", name: 'clueText', caption: "",
 	 components: [
 
 	     {kind: "Image", name: 'picture', src: ''},
@@ -29,19 +30,20 @@ enyo.kind({
 		   icon: 'resources/qmark.png', caption: 'Second try', style: "width: 100px;", onclick: 'checkLocation'},
 		  {kind: "IconButton", name: 'thirdTry', toggling: false, disabled: true,
 		   icon: 'resources/qmark.png', caption: 'Third try', style: "width: 100px;", onclick: 'checkLocation'},
+		  {kind: "IconButton", name: 'audioButton', icon: 'images/audio.png', caption: 'Listen', style: "width: 100px;", onclick: 'listen'}
 	      ]}]},
 	{kind: "ProgressBar", minimum: 1, maximum: 4, position: 3}],
     try1: function() {
 	
-		if( !this.checkLocation()) {
-			this.$.firstTry.setIcon( 'resources/cross.jpg' );
-		    this.$.firstTry.disabled = true;
-		    this.$.secondTry.disabled = false;
-		} else {
-		    this.nextQuestion();
-		}
+	if( !this.checkLocation()) {
+	    this.$.firstTry.setIcon( 'resources/cross.jpg' );
+	    this.$.firstTry.disabled = true;
+	    this.$.secondTry.disabled = false;
+	} else {
+	    this.nextQuestion();
+	}
     },
-	    try2: function() {
+    try2: function() {
 	if( !this.checkLocation() ) {
 	    this.$.secondTry.setIcon( 'resources/cross.jpg' );
 	    this.$.secondTry.disabled = true;
@@ -58,9 +60,12 @@ enyo.kind({
 	}
     },
     checkLocation: function() {
-		this.$[this.currentTry].disabled = true;
-    
-		this.$.getCurPosition.call();
+	this.$[this.currentTry].disabled = true;
+	
+	this.$.getCurPosition.call();
+    },
+    listen: function() {
+	this.$.soundPlayer.play();
     },
     nextQuestion: function() {
 	console.log( 'next question' );
@@ -72,6 +77,7 @@ enyo.kind({
 	var clue = this.$.clues.getCurrentClue();
 	this.$.clueText.setCaption( clue.text );
 	this.$.picture.setSrc( clue.image );
+	this.$.soundPlayer.setSrc( clue.sound );
 	this.resetButtons();
     },
     resetButtons: function() {
@@ -86,59 +92,57 @@ enyo.kind({
 	this.$.secondTry.setIcon('resources/qmark.png')
 	this.$.thirdTry.setIcon('resources/qmark.png')
     },
-	failed: function() {
-		this.$.clueText.setCaption("Loooooooser");
-	},
-	nextTry: function() {
-		console.log("NEXT TRY");
-		this.$[this.currentTry].setIcon( 'resources/cross.jpg' );
+    failed: function() {
+	this.$.clueText.setCaption("Loooooooser");
+    },
+    nextTry: function() {
+	console.log("NEXT TRY");
+	this.$[this.currentTry].setIcon( 'resources/cross.jpg' );
     	this.$[this.currentTry].disabled = true;
 
-		if(this.currentTry == 'firstTry') {
-			this.currentTry = "secondTry";
-		}else if(this.currentTry == 'secondTry') {
-			this.currentTry = "thirdTry";
-		} else  {
-			this.failed();
-		}
+	if(this.currentTry == 'firstTry') {
+	    this.currentTry = "secondTry";
+	}else if(this.currentTry == 'secondTry') {
+	    this.currentTry = "thirdTry";
+	} else  {
+	    this.failed();
+	}
 
     	this.$[this.currentTry].disabled = false;
-	},
-		gotResponse : function (inSender, inResponse) {
-			console.log("GOT RESPONSE");
-    		
-    	},
-    	posFinished: function(inSender, inResponse) {
+    },
+    gotResponse : function (inSender, inResponse) {
+	console.log("GOT RESPONSE");
+    	
+    },
+    posFinished: function(inSender, inResponse) {
     	//	this.$.pageHeader.setContent("location retrieved" + enyo.json.stringify(inResponse));
 
-    		var actualLat = Math.round(parseFloat(inResponse.latitude) * 100) / 100;
-    		var actualLong = Math.round(parseFloat(inResponse.longitude) * 100) / 100;
+    	var actualLat = Math.round(parseFloat(inResponse.latitude) * 100) / 100;
+    	var actualLong = Math.round(parseFloat(inResponse.longitude) * 100) / 100;
 
-    		console.log(actualLat + " " + actualLong);
-    		console.log(this.$.clues.getCurrentClue().lat  + " " + this.$.clues.getCurrentClue().lon);
+    	console.log(actualLat + " " + actualLong);
+    	console.log(this.$.clues.getCurrentClue().lat  + " " + this.$.clues.getCurrentClue().lon);
 
-    		console.log(actualLat == this.$.clues.getCurrentClue().lat);
-    		console.log(actualLong == this.$.clues.getCurrentClue().lon);
+    	console.log(actualLat == this.$.clues.getCurrentClue().lat);
+    	console.log(actualLong == this.$.clues.getCurrentClue().lon);
 
-			
+	
 
-    		var result = (actualLat == this.$.clues.getCurrentClue().lat && actualLong == this.$.clues.getCurrentClue().lon) ;
-
-
-			console.log("RESULT " + result);
-			if(result) {
-				this.nextQuestion();
-			} else {
-				this.nextTry();
-			}
-    	},
-    	posFail : function(inSender, inResponse) {
-    		this.$.pageHeader.setContent("location lookup failed");
-    	},
-    	getPos: function() {
-    		console.log("HERE");
-    		this.$.getCurPosition.call();
-    	}
+    	var result = (actualLat == this.$.clues.getCurrentClue().lat && actualLong == this.$.clues.getCurrentClue().lon) ;
 
 
+	console.log("RESULT " + result);
+	if(result) {
+	    this.nextQuestion();
+	} else {
+	    this.nextTry();
+	}
+    },
+    posFail : function(inSender, inResponse) {
+    	this.$.pageHeader.setContent("location lookup failed");
+    },
+    getPos: function() {
+    	console.log("HERE");
+    	this.$.getCurPosition.call();
+    }
 })
