4451843020b8d5338330f9db97e4a3139b4d8820
james-rodgers <james.rodgers@guardian.co.uk>
2011-07-10 14:24:21 +0100

    added sound to clues

--- source/Clues.js
+++ source/Clues.js
@@ -3,9 +3,9 @@ enyo.kind({
     kind: enyo.Control,
     currentClueNumber: 0,
     cluelist: [
-	{text: "Perhaps a meeting with Alan Rusbridger would help", lat: "51.53", lon: "-0.12", image: 'images/question_1.png'},
-      	{text: "Pub named after tabloid name for redundancy, \n an item of footwear that's on the other foot now", lat: "51.52", lon: "-0.12", image: 'images/question_2.png'},
-      	{text: "Road that shares the name with a famous prison", lat: "51.53", lon: "-0.12", image: 'images/question_3.png'},
+	{text: "Perhaps a meeting with Alan Rusbridger would help", lat: "51.53", lon: "-0.12", image: 'images/question_1.png', audio: 'resources/audio_1.mp3'},
+      	{text: "Pub named after tabloid name for redundancy, \n an item of footwear that's on the other foot now", lat: "51.52", lon: "-0.12", image: 'images/question_2.png', audio: 'resources/audio_2.mp3'},
+      	{text: "Road that shares the name with a famous prison", lat: "51.53", lon: "-0.12", image: 'images/question_3.png', audio: 'resources/audio_3.mp3'},
       	{text: "You've won!!", lat: "51.53", lon: "-0.12", image: 'images/win.png'}
     ],
     
